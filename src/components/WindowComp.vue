3d3acccd6cd9f83f5227584c0c17716a21f6a471
Alexandr Volmanov <C:\Users\al\AppData\Roaming\The Bat!>
2020-12-01 12:22:28 +0300

    fix

--- src/components/WindowComp.vue
+++ src/components/WindowComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box" :class="{active: active || isDrag}" :style="{ left: posLeft + 'px', top: posTop + 'px', width: width + 'px', height: height + 'px' , zIndex: -index + 10 }">
+  <div class="box" :class="{active: active || isDrag}" :style="{ left: posLeft + 'px', top: posTop + 'px', width: width + 'px', height: height + 'px' , zIndex: -index + box.length + 1 }">
     <div class="box-header" @mousedown="onGrab">
       <div class="box-header__text">{{ title }}</div>
       <div class="box-header__close" @click="removeBlock"></div>
